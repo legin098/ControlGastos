2d18a50e7d4038c2d93706522fd2258eb42d480f
Jesus Cruz <jesus.cruz@campusvirtual.aunarvillavicencio.edu.co>
2022-06-01 18:44:13 -0500

    Nuevo presupuesto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { Header } from "./components/Header";
 
 function App() {
+  const [presupuesto, setPresupuesto] = useState("");
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
   return (
-    <h1>Control de gastos</h1>
-  )
+    <div>
+      <Header
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NuevoPresupuesto } from "./NuevoPresupuesto";
+
+export const Header = ({
+  presupuesto,
+  setPresupuesto,
+  isValidPresupuesto,
+  setIsValidPresupuesto,
+}) => {
+  return (
+    <header>
+      <h1>Planificador de Gastos</h1>
+      {isValidPresupuesto ? (
+        <p>Control Presupuesto</p>
+      ) : (
+        <NuevoPresupuesto
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+        />
+      )}
+    </header>
+  );
+};
